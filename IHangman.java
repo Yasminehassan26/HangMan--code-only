5d4916abdb5d64ee1f3f0a07c949fabb942a0235
Yasminehassan26 <61247056+Yasminehassan26@users.noreply.github.com>
2020-02-28 21:44:49 +0200

    Update IHangman.java

--- IHangman.java
+++ IHangman.java
@@ -4,10 +4,7 @@ public interface IHangman {
 	 String[] words=new String[10] ;
 	
 	void setDictionary(String[] words);
-	/**
-	* Pick a random secret word from the dictionary and returns it
-	* @return secret word or null
-	*/
+	
 	String selectRandomSecretWord();
 	
 	String guess(Character c ) throws Exception;
